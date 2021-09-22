ea3f8ec88743efe4050a2551d1c86d1d54925cd4
Cody-Macedo <macedo.cody@gmail.com>
2021-09-22 16:22:54 +0200

    version 2 update

--- src/Partiel.php
+++ src/Partiel.php
@@ -75,7 +75,13 @@ final class Partiel
 
     public static function decimalToRomanVersion2($nb): string
     {
-        return 0;
+        $roman_special_centaine = array("C" => 100, "CD" => 400, "D" => 500, "CM" => 900);
+        $roman_special_dizaine = array("X" => 10, "XL" => 40, "L" => 50, "XC" => 90);
+        $roman_special_unite = array("I" => 1, "IV" => 4, "V" => 5, "IX" => 9);
+        $array_roman = array_merge($roman_special_centaine, $roman_special_dizaine);
+        $array_roman = array_merge($array_roman, $roman_special_unite);
+
+        return "";
     }
 
     public static function romanToDecimal($nb): int
